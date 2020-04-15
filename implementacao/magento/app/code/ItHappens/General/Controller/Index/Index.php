5b8e232708a267ba6af2e1d0e05b5a7cbff4aaad
gleisson <gleissontr@gmail.com>
2020-04-14 22:16:31 -0300

    Comitando dados do test

---
+++ implementacao/magento/app/code/ItHappens/General/Controller/Index/Index.php
@@ -0,0 +1,12 @@
+<?php
+/**@Companny itHappens ::author Gleisson Quixabeira **/ 
+
+namespace ItHappens\General\Controller\Index;
+
+use Magento\Framework\App\Action\Action as Action;
+
+class Index extends Action{
+	public function execute(){
+		echo 'Gleisson Quixabeira';
+	}
+}
---
+++ implementacao/magento/app/code/ItHappens/General/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**@Company Gleisson Quixabeira **/
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'ItHappens_General',
+    __DIR__
+);
+
