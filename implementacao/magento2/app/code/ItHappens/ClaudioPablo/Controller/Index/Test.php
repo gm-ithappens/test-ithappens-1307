941fec2bb2bbfc58cf6629d2df1a6416cdda0c24
Pablo Santos <claudiopablosilva@hotmail.com>
2020-05-25 21:28:15 -0300

    criação da action com meu nome

---
+++ implementacao/magento2/app/code/ItHappens/ClaudioPablo/Controller/Index/Test.php
@@ -0,0 +1,10 @@
+<?php
+namespace ItHappens\ClaudioPablo\Controller\Index;
+use Magento\Framework\App\Action\Action;
+class Test extends Action {
+
+    public function execute() {
+        echo "Claudio Pablo Silva Santos";
+    }
+
+}
