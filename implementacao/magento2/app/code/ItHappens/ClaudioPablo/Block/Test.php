00fcecf9a9ce97cd453d4ec2a4ec7379d4b62869
Pablo Santos <claudiopablosilva@hotmail.com>
2020-05-25 23:13:26 -0300

    criação da action com meu nome usando block e template

---
+++ implementacao/magento2/app/code/ItHappens/ClaudioPablo/Block/Test.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ItHappens\ClaudioPablo\Block;
+use \Magento\Framework\View\Element\Template;
+
+class Test extends Template {
+
+    public function getMyName() {
+        return "Claudio Pablo Silva Santos";
+    }
+}
--- implementacao/magento2/app/code/ItHappens/ClaudioPablo/Controller/Index/Test.php
+++ implementacao/magento2/app/code/ItHappens/ClaudioPablo/Controller/Index/Test.php
@@ -1,10 +1,13 @@
 <?php
 namespace ItHappens\ClaudioPablo\Controller\Index;
 use Magento\Framework\App\Action\Action;
+use Magento\Framework\Controller\ResultFactory;
+
 class Test extends Action {
 
     public function execute() {
-        echo "Claudio Pablo Silva Santos";
+        //echo "Claudio Pablo Silva Santos";
+        return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
     }
 
 }
