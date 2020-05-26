d509a0f88f033b58ebbdbdb657123198aea5b26a
Pablo Santos <claudiopablosilva@hotmail.com>
2020-05-26 11:31:55 -0300

    Informações do produto na tela

--- implementacao/magento2/app/code/ItHappens/ClaudioPablo/Controller/Index/Test.php
+++ implementacao/magento2/app/code/ItHappens/ClaudioPablo/Controller/Index/Test.php
@@ -2,12 +2,23 @@
 namespace ItHappens\ClaudioPablo\Controller\Index;
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\Controller\ResultFactory;
+use \Magento\Framework\App\Action\Context;
+use \Magento\Framework\View\Result\PageFactory;
 
 class Test extends Action {
 
+    protected $_pageFactory;
+
+    public function __construct(
+        Context $context,
+        PageFactory $pageFactory)
+    {
+        $this->_pageFactory = $pageFactory;
+        return parent::__construct($context);
+    }
+
     public function execute() {
-        //echo "Claudio Pablo Silva Santos";
-        return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+        return $this->_pageFactory->create();
     }
 
 }
